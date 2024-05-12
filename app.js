437fe5b219a9ca29af7c07d4a1144f28aee40473
BhargaviSheth <it.sheth.bhargavi@gmail.com>
2024-05-13 05:20:07 +0530

    commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const billAmount = document.querySelector('#bill-Amount')
-const checkbutton = document.querySelector('#check-button')
-const cashGiven = document.querySelector('#Cash-given')
+const billAmount = document.querySelector('#bill-Amount');
+console.log(billAmount)
+const checkbutton = document.querySelector('#check-button');
+const cashGiven = document.querySelector('#Cash-given');
+const message = document.querySelector("#error0message") ;
 
 checkbutton.addEventListener('click' , () => console.log("clicked"));
