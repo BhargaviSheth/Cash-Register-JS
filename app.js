2b3d08f4d15482417411b846932bc836fcff67da
BhargaviSheth <it.sheth.bhargavi@gmail.com>
2024-05-13 05:09:47 +0530

    getting element in js

---
+++ app.js
@@ -0,0 +1,5 @@
+const billAmount = document.querySelector('#bill-Amount')
+const checkbutton = document.querySelector('#check-button')
+const cashGiven = document.querySelector('#Cash-given')
+
+checkbutton.addEventListener('click' , () => console.log("clicked"));
