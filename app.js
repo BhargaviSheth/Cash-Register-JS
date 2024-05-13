c1cc6a85db911b0d547ae1224c08ee5ddbf57a02
BhargaviSheth <it.sheth.bhargavi@gmail.com>
2024-05-13 06:19:20 +0530

    change structure

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const billAmount = document.querySelector('#bill-Amount');
+const billAmount = document.querySelector('#bill-amount');
 console.log(billAmount)
 const checkbutton = document.querySelector('#check-button');
 const cashGiven = document.querySelector('#Cash-given');
 const message = document.querySelector("#error0message") ;
 
-checkbutton.addEventListener('click' , () => console.log("clicked"));
+checkbutton.addEventListener('click' , function validateBillandCashAmount(){
+  message.computedStyleMap.display = "none"
+  if(billAmount.value > 0){
+
+  }
+  else{
+    message.computedStyleMap.display="block"
+    message.innerHTML = "The Bill amount should be positive"
+  }
+});
