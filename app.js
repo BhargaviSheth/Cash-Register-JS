f756e475a04c58a1d1ec2ee38b70e32a6cd3b2eb
BhargaviSheth <it.sheth.bhargavi@gmail.com>
2024-05-13 06:20:14 +0530

    apply js

--- app.js
+++ app.js
@@ -1,16 +1,97 @@
-const billAmount = document.querySelector('#bill-amount');
-console.log(billAmount)
-const checkbutton = document.querySelector('#check-button');
-const cashGiven = document.querySelector('#Cash-given');
-const message = document.querySelector("#error0message") ;
+let bill,
+  cash,
+  diff = 0,
+  i,
+  j;
 
-checkbutton.addEventListener('click' , function validateBillandCashAmount(){
-  message.computedStyleMap.display = "none"
-  if(billAmount.value > 0){
+const cashInputDiv = document.querySelector(".cashDiv");
+const nextBtn = document.querySelector("#next");
+const calculateBtn = document.querySelector("#calculate");
 
-  }
-  else{
-    message.computedStyleMap.display="block"
-    message.innerHTML = "The Bill amount should be positive"
+const errorDiv = document.querySelector(".error");
+const table = document.querySelector("table");
+const amountDiv = document.querySelector("div.hidden");
+const amount = document.querySelector(".amount");
+
+const notesArray = [2000, 500, 100, 20, 10, 5, 1];
+let countArray = [0, 0, 0, 0, 0, 0, 0];
+
+nextBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let billInput = document.querySelector("#bill");
+
+  if (billInput.value === "" || billInput.value <= 0) {
+    errorDiv.innerText = "Error: Please enter the value appropriately.";
+    amountDiv.classList.add("hidden");
+    countArray = countArray.map((num) => 0);
+
+    showOutput();
+  } else {
+    bill = parseInt(billInput.value);
+
+    errorDiv.innerText = "";
+    cashInputDiv.style.display = "block";
+    calculateBtn.style.display = "block";
   }
 });
+
+calculateBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  let cashInput = document.querySelector("#cash");
+  let billInput = document.querySelector("#bill");
+
+  countArray = countArray.map((num) => 0);
+  showOutput();
+
+  if (
+    cashInput.value === "" ||
+    cashInput.value <= 0 ||
+    billInput.value === "" ||
+    billInput.value <= 0
+  ) {
+    errorDiv.innerText = "Error: Please enter the value appropriately.";
+    amountDiv.classList.add("hidden");
+  } else {
+    bill = parseInt(billInput.value);
+    cash = parseInt(cashInput.value);
+
+    errorDiv.innerText = "";
+    amountDiv.classList.add("hidden");
+
+    if (cash === bill) {
+      errorDiv.innerText = "No amount to be returned";
+    } else if (cash < bill) {
+      errorDiv.innerText = "Error: Cash given cannot be less than bill amount";
+    } else {
+      calculateChange();
+    }
+  }
+});
+
+const calculateChange = () => {
+  diff = parseInt(cash) - parseInt(bill);
+
+  amount.innerText = `Rs. ${diff}`;
+
+  for (i = 0; i < notesArray.length; i++) {
+    if (diff >= notesArray[i]) {
+      countArray[i] = Math.floor(diff / notesArray[i]);
+      diff -= countArray[i] * notesArray[i];
+    } else {
+      countArray[i] = 0;
+    }
+  }
+
+  showOutput();
+  amountDiv.classList.remove("hidden");
+};
+
+const showOutput = () => {
+  let tbody = table.tBodies[0];
+
+  for (j = 0; j < countArray.length; j++) {
+    tbody.rows[j + 1].cells[1].innerHTML = countArray[j];
+  }
+};
